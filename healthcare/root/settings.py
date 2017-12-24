cf1ba4c9ee5afee8686da8bb8cbcb743346701d4
tejayerraballe <teja.yerraballe@aspiresys.com>
2017-12-25 01:02:48 +0530

    intial commit

--- healthcare/root/settings.py
+++ healthcare/root/settings.py
@@ -44,13 +44,14 @@ INSTALLED_APPS = [
     'rest_framework_mongoengine',
     'mongoengine.django.mongo_auth',
     'users',
+    'block_chain'
 ]
 
 MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
